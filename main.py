23aa3cb7ee1c303cd8f259bf4698e28084b70da9
JefferyBurgett <jeffery.burgett@gmail.com>
2022-06-04 17:29:12 -0500

    Added Locations Input

--- main.py
+++ main.py
@@ -4,20 +4,42 @@
 
 locations_list = ["Madison", "Green Bay", "Wisconsin Dells", "Chicago"]
 
-madison_restaurants_list = ["Sala Thai", "Sequoia", "Off Boradway", "Buck & Honey's"]
+#madison_restaurants_list = ["Sala Thai", "Sequoia", "Off Boradway", "Buck & Honey's"]
 
-green_bay_restaurants_list = ["Angelina", "Sushi Lover", "Our Place", "Al's Hamburger"]
+#green_bay_restaurants_list = ["Angelina", "Sushi Lover", "Our Place", "Al's Hamburger"]
 
-wisconsin_dells_restaurants_list = ["Paul Bunyan's", "Mexicali Rose", "Monk's", "Showboat Saloon"]
+#wisconsin_dells_restaurants_list = ["Paul Bunyan's", "Mexicali Rose", "Monk's", "Showboat Saloon"]
 
-chicago_restaurants_list = ["Portillo's", "Gibsons", "Gyu-Kaku", "Buona"]
+#chicago_restaurants_list = ["Portillo's", "Gibsons", "Gyu-Kaku", "Buona"]
 
 transportation = ["Car","Train","Motorcycle","Bus"]
 
-madison_entertainment_list = ["Henry Vilas Zoo","Geology Museum","Olbrich Botanical Gardens","Capitol Tour"]
+#madison_entertainment_list = ["Henry Vilas Zoo","Geology Museum","Olbrich Botanical Gardens","Capitol Tour"]
 
-green_bay_entertainment_list = ["Packers Hall of Fame","National Railroad Museum","Bay Beach Wildlife Sanctuary","Fox River Trail"]
+#green_bay_entertainment_list = ["Packers Hall of Fame","National Railroad Museum","Bay Beach Wildlife Sanctuary","Fox River Trail"]
 
-wisconsin_dells_entertainment_list = ["Noah's Ark","Ho-Chunk","Tommy Bartlett","Timber Falls Adventure Park"]
+#wisconsin_dells_entertainment_list = ["Noah's Ark","Ho-Chunk","Tommy Bartlett","Timber Falls Adventure Park"]
 
-chicago_entertainment_list = ["Grant Park","Art Institute","Willis Tower","Shedd Aquarium"]
+#chicago_entertainment_list = ["Grant Park","Art Institute","Willis Tower","Shedd Aquarium"]
+
+#confirmed_trip_list = ["confirmed_location", "confirmed_transportation"]
+
+restaurants = ["Wendy's","Culver's","Burger King","Chipotle"]
+import random
+
+def random_item_picker(list):
+    random_item = random.choice(list)
+    return random_item
+
+
+def random_location_picker():
+    random_location = random_item_picker(locations_list)
+    
+    user_confirmed = False
+    while user_confirmed == False:
+        user_input = input(f'Would you like to keep {random_location} as your destination? y/n ')
+        if user_input == "y":
+            print(f'You have choosen to keep {random_location} as your destination.')
+            return random_location
+        elif user_input == "n":
+            return random_location_picker()
