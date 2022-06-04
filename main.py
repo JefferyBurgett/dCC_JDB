e19e21f3a834457b40c4ec8ac007647b6d5ae0d1
JefferyBurgett <jeffery.burgett@gmail.com>
2022-06-04 16:37:26 -0500

    Added DTG Lists

---
+++ main.py
@@ -0,0 +1,23 @@
+#DayTripGenerator
+
+#Lists
+
+locations_list = ["Madison", "Green Bay", "Wisconsin Dells", "Chicago"]
+
+madison_restaurants_list = ["Sala Thai", "Sequoia", "Off Boradway", "Buck & Honey's"]
+
+green_bay_restaurants_list = ["Angelina", "Sushi Lover", "Our Place", "Al's Hamburger"]
+
+wisconsin_dells_restaurants_list = ["Paul Bunyan's", "Mexicali Rose", "Monk's", "Showboat Saloon"]
+
+chicago_restaurants_list = ["Portillo's", "Gibsons", "Gyu-Kaku", "Buona"]
+
+transportation = ["Car","Train","Motorcycle","Bus"]
+
+madison_entertainment_list = ["Henry Vilas Zoo","Geology Museum","Olbrich Botanical Gardens","Capitol Tour"]
+
+green_bay_entertainment_list = ["Packers Hall of Fame","National Railroad Museum","Bay Beach Wildlife Sanctuary","Fox River Trail"]
+
+wisconsin_dells_entertainment_list = ["Noah's Ark","Ho-Chunk","Tommy Bartlett","Timber Falls Adventure Park"]
+
+chicago_entertainment_list = ["Grant Park","Art Institute","Willis Tower","Shedd Aquarium"]
