75bba9babd6497f0e09ff6f67082fea084d5cd38
JefferyBurgett <jeffery.burgett@gmail.com>
2022-06-13 15:47:59 -0500

    Cut Out Excess Lists

--- main.py
+++ main.py
@@ -4,27 +4,11 @@
 
 locations_list = ["Madison", "Green Bay", "Wisconsin Dells", "Chicago"]
 
-#madison_restaurants_list = ["Sala Thai", "Sequoia", "Off Boradway", "Buck & Honey's"]
+transportations_list = ["Car","Train","Motorcycle","Bus"]
 
-#green_bay_restaurants_list = ["Angelina", "Sushi Lover", "Our Place", "Al's Hamburger"]
+restaurants_list = ["Wendy's","Culver's","Burger King","Chipotle"]
 
-#wisconsin_dells_restaurants_list = ["Paul Bunyan's", "Mexicali Rose", "Monk's", "Showboat Saloon"]
-
-#chicago_restaurants_list = ["Portillo's", "Gibsons", "Gyu-Kaku", "Buona"]
-
-transportation = ["Car","Train","Motorcycle","Bus"]
-
-#madison_entertainment_list = ["Henry Vilas Zoo","Geology Museum","Olbrich Botanical Gardens","Capitol Tour"]
-
-#green_bay_entertainment_list = ["Packers Hall of Fame","National Railroad Museum","Bay Beach Wildlife Sanctuary","Fox River Trail"]
-
-#wisconsin_dells_entertainment_list = ["Noah's Ark","Ho-Chunk","Tommy Bartlett","Timber Falls Adventure Park"]
-
-#chicago_entertainment_list = ["Grant Park","Art Institute","Willis Tower","Shedd Aquarium"]
-
-#confirmed_trip_list = ["confirmed_location", "confirmed_transportation"]
-
-restaurants = ["Wendy's","Culver's","Burger King","Chipotle"]
+entertainments_list = ["Hiking","Concert","Shopping","Cinema"]
 import random
 
 def random_item_picker(list):
@@ -39,7 +23,7 @@ def random_location_picker():
     while user_confirmed == False:
         user_input = input(f'Would you like to keep {random_location} as your destination? y/n ')
         if user_input == "y":
-            print(f'You have choosen to keep {random_location} as your destination.')
+            print(f'You have chose to keep {random_location} as your destination.')
             return random_location
         elif user_input == "n":
             return random_location_picker()
@@ -47,13 +31,13 @@ confirmed_location = random_location_picker()
 print(confirmed_location)
 
 def random_transportation_picker():
-    random_transportation = random_item_picker(transportation)
+    random_transportation = random_item_picker(transportations_list)
 
     user_confirmed_one = False
     while user_confirmed_one == False:
         user_input = input(f'Would you like to keep {random_transportation} as your mode of transportation? y/n ')
         if user_input == "y":
-            print(f'You have choosen to keep {random_transportation} as your mode of transportation.')
+            print(f'You have chose to keep {random_transportation} as your mode of transportation.')
             return random_transportation
         elif user_input == "n":
             return random_transportation_picker()
@@ -62,16 +46,33 @@ confirmed_transportation = random_transportation_picker()
 print(confirmed_transportation)
 
 def random_restaurant_picker():
-    random_restaurant = random_item_picker(restaurants)
+    random_restaurant = random_item_picker(restaurants_list)
 
     user_confirmed = False
     while user_confirmed == False:
         user_input = input(f'Would you like to keep {random_restaurant} as your eating destination? y/n ')
         if user_input == "y":
-            print(f'You have choosen to keep {random_restaurant} as your mode of destination.')
+            print(f'You have chose to keep {random_restaurant} as your mode of destination.')
             return random_restaurant
         elif user_input == "n":
             return random_restaurant_picker()
 
 confirmed_restaurant = random_restaurant_picker()
 print(confirmed_restaurant)
+
+def random_entertainment_picker():
+    random_entertainment = random_item_picker(entertainments_list)
+    
+    user_confirmed = False
+    while user_confirmed == False:
+        user_input = input(f'Would you like to keep {random_entertainment} as your entertainment choice? y/n ')
+        if user_input == "y":
+            print(f'You have chose to keep {random_entertainment} as your choice of entertainment')
+            return random_entertainment
+        elif user_input == "n":
+            return random_entertainment_picker()
+
+confirmed_entertainment = random_entertainment_picker()
+print(confirmed_entertainment)
+
+print(f"You have chosen {confirmed_location}, {confirmed_transportation}, {confirmed_restaurant} and {confirmed_entertainment} for your Day Trip")
